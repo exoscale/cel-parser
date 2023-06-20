06e4994e7ceb68a949d3f502db554acf369925aa
Alex King <alex@alexanderjamesking.com>
2023-06-20 16:26:10 +0200

    Fix zero-based indexed day of week/month/year (#9)
    As per https://github.com/google/cel-spec/blob/master/doc/langdef.md

--- test/exoscale/cel/timestamps_test.clj
+++ test/exoscale/cel/timestamps_test.clj
@@ -4,12 +4,70 @@
    [exoscale.cel.parser :as parser]
    [exoscale.cel.test-helper :as helper]))
 
-(deftest ensure-datetime-conversion-24
-  (testing
-   "Ensure that getHours returns 24 formated hours"
-    (is (-> (parser/parse-eval
-             {:bindings (helper/bindings nil), :translate-result? false}
-             "timestamp('2009-02-13T23:31:30Z').getHours()")
-            :x
-            (= 23)))))
+(defn parse-eval [expr]
+  (:x (parser/parse-eval
+       {:bindings (helper/bindings nil), :translate-result? false}
+       expr)))
+
+(defn timestamp-fn [f datetime]
+  (parse-eval (str "timestamp('" datetime "')." f "()")))
+
+(deftest timestamp-fn-test
+  (let [get-day-of-week (partial timestamp-fn "getDayOfWeek")
+        get-day-of-month (partial timestamp-fn "getDayOfMonth")
+        get-day-of-year (partial timestamp-fn "getDayOfYear")
+        get-month (partial timestamp-fn "getMonth")
+        get-full-year (partial timestamp-fn "getFullYear")
+        get-hours (partial timestamp-fn "getHours")
+        get-minutes (partial timestamp-fn "getMinutes")
+        get-seconds (partial timestamp-fn "getSeconds")
+        get-date (partial timestamp-fn "getDate")]
+
+    (testing "getDayOfWeek: get day of week from the date in UTC, zero-based, zero for Sunday"
+      (is (= 0 (get-day-of-week "2023-06-18T23:31:30Z")))
+      (is (= 1 (get-day-of-week "2023-06-19T23:31:30Z")))
+      (is (= 2 (get-day-of-week "2023-06-20T23:31:30Z")))
+      (is (= #inst "2023-06-21T00:31:30.021000000-00:00"
+             (parse-eval "timestamp('2023-06-20T23:31:30.021-01:00')")))
+      (is (= 3 (parse-eval "timestamp('2023-06-20T23:31:30.021-01:00').getDayOfWeek()")))
+      (is (= 2 (parse-eval "timestamp('2023-06-20T21:31:30Z').getDayOfWeek('CET')")))
+      (is (= 3 (parse-eval "timestamp('2023-06-20T22:31:30Z').getDayOfWeek('CET')")))
+      (is (= 2 (parse-eval "timestamp('2023-06-20T17:31:30Z').getDayOfWeek('IST')")))
+      (is (= 3 (parse-eval "timestamp('2023-06-20T18:31:30Z').getDayOfWeek('IST')")))
+      (is (= 3 (parse-eval "timestamp('2023-06-20T23:31:30Z').getDayOfWeek('IST')"))))
+
+    (testing "getDayOfMonth: get day of month from the date in UTC, zero-based indexing"
+      (is (= 0 (get-day-of-month "2023-06-01T23:31:30Z"))))
+
+    (testing "getDate: get day of month from the date with timezone, one-based indexing"
+      (is (= 1 (get-date "2023-06-01T23:00:00Z")))
+      (is (= 2 (get-date "2023-06-02T23:00:01Z"))))
+
+    (testing "getDayOfYear: get day of year from the date in UTC, zero-based indexing"
+      (is (= 0 (get-day-of-year "2023-01-01T23:31:30Z"))))
+
+    (testing "getMonth: get month from the date in UTC, 0-11"
+      (is (= 0 (get-month "2023-01-01T23:31:30Z")))
+      (is (= 7 (get-month "2023-08-01T23:31:30Z")))
+      (is (= 11 (get-month "2023-12-31T23:31:30Z"))))
+
+    (testing "getFullYear:  get year from the date in UTC"
+      (is (= 2023 (get-full-year "2023-01-01T23:31:30Z")))
+      (is (= 1979 (get-full-year "1979-03-08T00:31:30Z"))))
+
+    (testing "getHours: get hours from the date in UTC, 0-23"
+      (is (= 23 (get-hours "2023-06-20T23:31:30Z")))
+      (is (= 17 (get-hours "2023-06-20T17:00:30Z")))
+      (is (= 0 (get-hours "2023-06-20T00:00:00Z"))))
+
+    (testing "getMinutes: get minutes from the date in UTC, 0-59"
+      (is (= 31 (get-minutes "2023-06-20T23:31:30Z")))
+      (is (= 0 (get-minutes "2023-06-20T23:00:30Z")))
+      (is (= 59 (get-minutes "2023-06-20T23:59:30Z"))))
+
+    (testing "getSeconds: get seconds from the date in UTC, 0-59"
+      (is (= 0 (get-seconds "2023-06-20T23:00:00Z")))
+      (is (= 1 (get-seconds "2023-06-20T23:00:01Z")))
+      (is (= 33 (get-seconds "2023-06-20T23:00:33Z")))
+      (is (= 59 (get-seconds "2023-06-20T23:00:59Z"))))))
 
